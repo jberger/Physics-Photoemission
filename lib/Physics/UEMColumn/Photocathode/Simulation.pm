dd6a470f0b40908e3b27773d717564a06ab9d75f
Joel Berger <joel.a.berger@gmail.com>
2013-02-04 12:05:49 -0600

    more subclass doc

--- lib/Physics/UEMColumn/Photocathode/Simulation.pm
+++ lib/Physics/UEMColumn/Photocathode/Simulation.pm
@@ -22,6 +22,12 @@ Physics::UEMColumn::Photocathode::Simulation - Subclass of Physics::UEMColumn::P
 
   my $pulse = $photocathode->generate_pulse( 1e8 );
 
+=head1 DESCRIPTION
+
+This class is a subclass of L<Physics::UEMColumn::Photocathode> which uses L<Physics::Photoemission> to generate the longitudinal initial conditions of the generated pulse.
+
+Since it is as subclass, it implements all the attributes and methods of that class, with the following changes.
+
 =cut
 
 use Moo;
@@ -32,13 +38,37 @@ use Physics::Photoemission::Constants qw/charge/;
 
 use Carp;
 
+=head1 PACKAGE VARIABLES
+
+=over
+
+=item C<$Emission_Time>
+
+The simulation allows a small amount of time to run off in generating the pulse. This package variable contains the amount of run off time for the last generation. One might want to localize this variable if it is to be used to correct the timing of a L<Physics::UEMColumn> simulation. The value is simply calculated by multiplying C<num_taus> and C<tau> from L<Physics::Photoemission>.
+
+=cut
+
 our $Emission_Time = 0;
+
+=item C<$Anon_Class_Num>
+
+This number is incremented for each anonymous subclass created (see L</IMPORTING>).
+
+=back
+
+=cut
+
 our $Anon_Class_Num = 0;
 
-has 'simulation_overrides' => (
-  is => 'lazy',
-);
-sub _build_simulation_overrides { +{} }
+=head1 ATTRIBUTES
+
+=over
+
+=item C<simulation>
+
+Holder for the L<Physics::Photoemission> object. It is generated lazily when needed. Its C<tau>, C<work_function>, C<photon_energy> and C<field> are loaded from the information in the surrounding simulation. Additional overrides (see below) are merged with higher priority.
+
+=cut
 
 has 'simulation' => (
   is => 'lazy',
@@ -59,6 +89,31 @@ sub _build_simulation {
     %{ $self->simulation_overrides },
   );
 }
+
+=item C<simulation_overrides>
+
+A hash reference merged with the automatic attributes to the C<simulation> object (above). These attributes are merged with higher priority than the automatic ones should you want to override those too.
+
+=back
+
+=cut
+
+has 'simulation_overrides' => (
+  is => 'lazy',
+);
+sub _build_simulation_overrides { +{} }
+
+=head1 METHODS
+
+=over
+
+=item C<generate_pulse>
+
+Overrides the method in L<Physics::UEMColumn::Photocathode>, though that method is still called in order to get the transverse conditions. This method is called by the L<Physics::UEMColumn> simulation to generate the pulse object (if necessary).
+
+=back
+
+=cut
 
 sub generate_pulse {
   my ($self, $num) = @_;
@@ -89,6 +144,32 @@ sub generate_pulse {
 
   return $pulse;
 }
+
+=head1 IMPORTING
+
+As with L<Physics::UEMColumn|Physics::UEMColumn/"IMPORTING"> this class can create an alias for easier use in your scripts. The calling convention is slightly different however since there is only one class (and its a subclass of one likely to be aliased). Any string passed as an import argument will be aliased to the name C<Physics::UEMColumn::Photocathode::Simulation>. In this documentation and examples the alias is by convention C<SimPhotocathode>.
+
+ use Physics::UEMColumn alias => ':standard';
+ use Physics::UEMColumn::Photocathode::Simulation 'SimPhotocathode';
+ my $pulse = SimPhotocathode->new( ... );
+
+As a further convenience, if a second arguement (a hash reference) is passed, that will be used as the default value of C<simulation_overrides>. This is accomplished by creating an "anonymous" subclass of this package and storing the name in the created alias.
+
+ use Physics::UEMColumn alias => ':standard';
+ use Physics::UEMColumn::Photocathode::Simulation 'SimPhotocathode' => {
+   num_space_bins  => 100,
+   num_time_slices => 100,
+ };
+ my $pulse = SimPhotocathode->new( ... );
+ 
+ print ref $pulse; # Physics::UEMColumn::Photocathode::Simulation::Anon0
+
+Finally for symmetry, an initial key "alias" is silently ignored in any case.
+
+ use Physics::UEMColumn alias => ':standard';
+ use Physics::UEMColumn::Photocathode::Simulation alias => 'SimPhotocathode';
+
+=cut
 
 # hic sunt dracones
 sub import {
@@ -122,4 +203,20 @@ sub import {
 }
 
 1;
+
+=head1 SOURCE REPOSITORY
+
+L<http://github.com/jberger/Physics-Photoemission>
+
+=head1 AUTHOR
+
+Joel Berger, E<lt>joel.a.berger@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2013 by Joel Berger
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
 
